8ef9888ee26020c269783415a4e4ccb8f3830beb
Bram Duvigneau <bram@bramd.nl>
2020-08-26 23:20:25 +0200

    Create volume0/1 mountpoints before mounting

--- data/cloudinit-bootstrap.sh
+++ data/cloudinit-bootstrap.sh
@@ -14,7 +14,9 @@ if [ $(echo -n ${volume0_dev} | wc -c) -gt 8 ]; then
     if [ $(cat /etc/fstab | grep "${volume0_mount}" | wc -l) -lt 1 ]; then
         echo $(lsblk --fs --output UUID --noheadings --pairs ${volume0_dev})' '${volume0_mount}' '${volume0_fstype}' defaults 0 2' >> /etc/fstab
     fi
-    mount --all
+    echo "Trying to mount ${volume0_mount}"
+    mkdir ${volume0_mount}
+    mount "${volume0_mount}"
 fi;
 
 # volume1 - if defined
@@ -25,5 +27,6 @@ if [ $(echo -n ${volume1_dev} | wc -c) -gt 8 ]; then
     if [ $(cat /etc/fstab | grep "${volume1_mount}" | wc -l) -lt 1 ]; then
         echo $(lsblk --fs --output UUID --noheadings --pairs ${volume1_dev})' '${volume1_mount}' '${volume1_fstype}' defaults 0 2' >> /etc/fstab
     fi
-    mount --all
+    mkdir ${volume1_mount}
+    mount "${volume1_mount}"
 fi;
